c77a4947a53102bb487faba78ab30c06c25d29b4
Vladimir Siedykh <datrimius@gmail.com>
2025-01-16 10:10:24 +0100

    Refactor project structure and enhance styling with Tailwind CSS
    - Updated ESLint configuration to use single quotes for consistency.
    - Modified Next.js configuration to use single quotes.
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, next-themes, tailwind-merge, and tailwindcss-animate.
    - Enhanced Tailwind CSS configuration with new themes, animations, and responsive design utilities.
    - Updated global styles to include new utility classes and removed scrollbar styles.
    - Refactored layout and page components to improve structure and styling.
    - Removed unused SVG files and favicon.
    - Added Prettier configuration for code formatting.
    - Updated package-lock.json to reflect new dependencies and versions.

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
