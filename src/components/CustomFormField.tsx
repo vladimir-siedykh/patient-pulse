ae923c29fa33d73b66309397ddedc56eb12fbcc7
Vladimir Siedykh <datrimius@gmail.com>
2025-01-16 12:30:53 +0100

    feat(form): enhance PatientForm with validation and layout
    - add: Zod schema validation
    - add: react-hook-form integration
    - add: custom form fields and layout
    - fix: hydration warnings in RootLayout

---
+++ src/components/CustomFormField.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Control } from 'react-hook-form';
+import { FormFieldType } from './forms/PatientForm';
+import Image from 'next/image';
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+  autoComplete?: string;
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder, autoComplete } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || 'icon'}
+              className='ml-2'
+            />
+          )}
+          <FormControl>
+            <Input
+              
+              placeholder={placeholder}
+              {...field}
+              className='shad-input border-0'
+              autoComplete={autoComplete}
+            />
+          </FormControl>
+        </div>
+      );
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className='flex-1'>
+          {fieldType !== FormFieldType.CHECKBOX && label && <FormLabel>{label}</FormLabel>}
+
+          <RenderField field={field} props={props} />
+          <FormMessage className='shad-error' />
+        </FormItem>
+      )}
+    />
+  );
+};
+export default CustomFormField;
--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -1,6 +1,65 @@
-const PatientForm = () => {
-  return (
-    <div>PatientForm</div>
-  )
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import CustomFormField from '../CustomFormField';
+
+export enum FormFieldType {
+  INPUT = 'input',
+  CHECKBOX = 'checkbox',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
 }
-export default PatientForm
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+});
+
+const PatientForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='flex-1 space-y-6'>
+        <section className='mb-12 space-y-4'>
+          <h1 className='header'>Hi there 👋</h1>
+          <p className='text-dark-700'>Schedule your first appointment with us</p>
+        </section>
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name='name'
+          label='Full name'
+          placeholder='Enter your full name'
+          iconSrc='/assets/icons/user.svg'
+          iconAlt='user icon'
+          autoComplete='name'
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
