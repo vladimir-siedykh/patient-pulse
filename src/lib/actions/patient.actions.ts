b06d17c37b3d6110ad7f40cad9d3eea6f48f189d
Vladimir Siedykh <datrimius@gmail.com>
2025-01-19 13:57:31 +0100

    feat(form): refactor RegisterForm for patient registration and enhance submission logic
    - Updated RegisterForm to utilize PatientFormValidation for improved validation.
    - Enhanced form submission to include additional patient details such as birth date, gender, and medical history.
    - Integrated file upload functionality for identification documents.
    - Replaced createUser with registerPatient action for patient registration.
    - Updated routing to redirect to new appointment page upon successful registration.

--- src/lib/actions/patient.actions.ts
+++ src/lib/actions/patient.actions.ts
@@ -1,8 +1,18 @@
 'use server';
 import { ID, Query } from 'node-appwrite';
-import { users } from '../appwrite.config';
+import {
+  BUCKET_ID,
+  PROJECT_ID,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  ENDPOINT,
+  storage,
+  users,
+  databases,
+} from '../appwrite.config';
 import { AppwriteException } from 'node-appwrite';
 import { parseStringify } from '../utils';
+import { InputFile } from 'node-appwrite/file';
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -30,5 +40,43 @@ export const getUser = async (userId: string) => {
     return parseStringify(user);
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    let file;
+    if (identificationDocument) {
+      const inputFile =
+        identificationDocument &&
+        InputFile.fromBuffer(
+          identificationDocument?.get('blobFile') as Blob,
+          identificationDocument?.get('fileName') as string
+        );
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id ? file.$id : null,
+        identificationDocumentUrl: file?.$id
+          ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+          : null,
+        ...patient,
+      }
+    );
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error('An error occurred while creating a new patient:', error);
   }
 };
